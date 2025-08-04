b24d178a31a516bc2d8e99c8193a7ac81e75cebe
aayush kumar <aayushhpandey@gmail.com>
2025-08-04 09:32:07 +0530

    page num

--- pages/search.py
+++ pages/search.py
@@ -10,6 +10,9 @@ st.title("Apollo.io People Extractor")
 
 api_key = st.text_input(" Enter your Apollo API Key", type="password")
 apollo_ui_url = st.text_area(" Paste Apollo People Search URL", height=100)
+numberpages = st.number_input("Number of pages to fetch", min_value=1, max_value=100, value=1)
+perpage = st.number_input("Number of results per page", min_value=1, max_value=100, value=100)
+
 def send_file_to_webhook(file_bytes: bytes, filename: str):
     url = "https://bizmaxus.app.n8n.cloud/webhook/csv"
     files = {"data": (filename, BytesIO(file_bytes), "application/octet-stream")}
@@ -28,7 +31,7 @@ if st.button("🔍 Fetch Leads"):
             # Extract query string from Apollo UI URL
             parsed = urlparse(apollo_ui_url)
             query_string = parsed.fragment.replace('/people?', '')
-            api_url = f"https://api.apollo.io/api/v1/mixed_people/search?{query_string}&per_page=100&page=1"
+            api_url = f"https://api.apollo.io/api/v1/mixed_people/search?{query_string}&per_page={perpage}&page={numberpages}"
 
             headers = {
                 "x-api-key": api_key,
